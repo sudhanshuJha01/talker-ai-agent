bf21b03fba9b286dcb02889ef95550e0f7929832
sudhanshuJha01 <sudhanshujha883@gmail.com>
2025-10-08 00:25:04 +0530

    agent basic chat build completed

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-import readline from 'node:readline/promises'
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import readline from "node:readline/promises";
+import { ChatGroq } from "@langchain/groq";
+import dotenv from 'dotenv'
+dotenv.config();
 
-const r1 = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+/**
+ * node define
+ * graph workflow
+ *  complile and invoke
+ */
+
+const model = new ChatGroq({
+    model:"openai/gpt-oss-120b",
+    temperature:0,
+    maxRetries:2
 })
-
-const main = async ()=>{
-
-    while(true){
-        const prompt =await r1.question("You : ")
-        if(prompt ==='/bye') break;
-        console.log("your question " , prompt);
-    }
-
-    r1.close();
-    
+async function callModel(state) {
+    const response = await model.invoke(state.messages);
+  return { messages: [response] };
 }
+
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge("__start__", "agent")
+  .addEdge("agent", "__end__");
+
+const app = workflow.compile();
+
+const main = async () => {
+  const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  while (true) {
+    const prompt = await r1.question("You : ");
+    if (prompt === "/bye") break;
+
+    const finalState = await app.invoke({
+      messages: [{role:"user" , content:prompt}],
+    });
+    const aiResponse = finalState.messages[finalState.messages.length - 1].content
+    console.log("FinalState ", aiResponse);
+  }
+
+  r1.close();
+};
 
 main();
